44c3b298ec7e5c1a7ade9054c9ed217ee1170db4
Roger Jamsek <rowdyroom@gmail.com>
2026-06-28 21:21:10 -0500

    Export provider routing modules

--- packages/ai-router/src/index.ts
+++ packages/ai-router/src/index.ts
@@ -1,2 +1,6 @@
 export * from "./types.js";
 export * from "./router.js";
+export * from "./provider-client.js";
+export * from "./provider-registry.js";
+export * from "./provider-config.js";
+export * from "./routed-completion.js";
